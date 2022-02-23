31c5a48b389a335c3ba004d494a47d61083b09da
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-02-24 00:11:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Emp_Wage_With_OOPS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Calculation problem solving with OOPS concept");
+        }
+    }
+}
