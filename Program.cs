d96d997e7278bf834a03a5694f6acf16eebcdf92
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-24 16:31:58 +0530

    Wage_For_Multiple_Companies

--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@ namespace EmployeeWageComputation
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Employee Wage Calculation problem solving with OOPS concept");
+       public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Employee Wage Calculation problem solving with OOPS concept");
 
-            EmployeeWage employeeWage = new EmployeeWage();
-            employeeWage.GetEmpWage();
-
-        }
+        EmployeeWage employeeWage1 = new EmployeeWage("Dmart", 20, 100, 20);
+        EmployeeWage employeeWage2 = new EmployeeWage("Tata", 25, 125, 30);
+        employeeWage1.GetEmpWage();
+        employeeWage2.GetEmpWage();
+    }
     }
 }
